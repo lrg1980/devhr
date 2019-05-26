1cd99f16cbbf48ddc0a1a15e8e82ceff12ac00cf
lrg1980 <admin@binweb.net>
2019-05-26 18:08:40 -0300

    Agregamos vacantes desde DB

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
-exports.mostrarTrabajos = (req, res) => {
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
+
+exports.mostrarTrabajos = async (req, res, next) => {
+
+     const vacantes = await Vacante.find();
+
+     if (!vacantes) return next();
+
      res.render('home', {
           nombrePagina: 'devHR',
           tagline: 'Encuentra y publica búsquedas laborales para desarrolladores',
           barra: true,
-          boton: true
+          boton: true,
+          vacantes
      })
 }
